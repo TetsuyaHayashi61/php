e8c9cbe8d7edc714d27af035d56cc28339053b0a
TetsuyaHayashi61 <tetsuya_zheye@hotmail.com>
2015-09-04 23:51:18 +0900

    9/4 commit

--- beans.php
+++ beans.php
@@ -8,24 +8,24 @@
 <body>
     <?php
     try {
-      $pdo = new PDO(`mysql:host=localhost;dbname=beans,charaset=utf8`,'root',``,
+      $pdo = new PDO('mysql:host=localhost;dbname=beans,charaset=utf8','root','',
       array(PDO::ATTR_EMULATE_PREPARES => false));
     } catch (PDOException $e) {
-      exit(`データベース接続失敗。`.$e->getMessage());
+      exit('データベース接続失敗。'.$e->getMessage());
     }
 
-    /*
-      $dsn = `mysql:dbname=beans,host=localhost,`;
-      $user = `root`;
-      $password =  ``;
+/*
+      $dsn = 'mysql:dbname=beans,host=localhost,';
+      $user = 'root';
+      $password =  '';
 
       try {
         $dbh = new PDO($dsn,$user,$password);
       } catch (PDOException $e) {
-        print(`データベース接続失敗。`.$e->getMessage());
+        print('データベース接続失敗。'.$e->getMessage());
         die();
       }
-    */
+*/
 
     ?>
 
